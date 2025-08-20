fba57d24ef6cb7f1ed5d44f48cc05e2ce7ab1aea
Fahmi Dhika <fahmidhika810@gmail.com>
2025-08-20 13:54:31 +0700

    revisi urutan produk, deskripsi produk, ukuran card produk

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import logo from "../public/assets/logo panjang.png";
 import Link from "next/link";
 import whatsapp from "../public/assets/whatsapp.png";
 import Footer from "../components/footer";
@@ -12,60 +11,65 @@ import Header from "@/components/header";
 export default function Page() {
   const produk = [
     {
-      foto: "/assets/jwstv.jpg",
-      nama: "Jam Shalat Waktu TV",
-      harga: 1950000,
-      tag: "Produk Paling Unik",
-      deskripsi:
-        "Jam Waktu Shalat yang bisa di tampilkan di TV. Merupakan alat android portable yang bisa dibawa kemana mana dan fleksibel ditaruh dimana saja.",
-    },
-    {
-      foto: "/assets/mesinautotartil.jpg",
-      nama: "Mesin Auto Tartil",
-      harga: 850000,
-      tag: "Produk Bagus",
-      deskripsi:
-        "Player TARTIL/TARHIM otomatis menjelang waktu sholat, terintegrasi dengan system audio masjid sehingga amplifier bisa auto on/off sesuai program atau waktu yang ditentukan.",
-    },
-    {
-      foto: "/assets/papanskor.jpeg",
-      nama: "Papan Skor",
-      harga: 1350000,
-      tag: "Produk Terbaik",
-      deskripsi:
-        "Papan skor berukuran 20x70cm. Menampilkan skor kedua tim cocok untuk dipasang pada pertandingan speak bola, voli, dan lainnya. Selain itu juga menampilkan timer untuk waktu permainan, dan juga nama tim di tiap sisi.",
-    },
-    {
       foto: "/assets/jws1.jpeg",
       nama: "Jam Dinding Digital Islami",
       harga: 450000,
       tag: "Produk Terjangkau",
+      terjual: 150,
       deskripsi:
-        "Jam dinding yang memiliki desain unik dan ukuran yang relatif kecil. Tersedia dalam 2 jenis, dengan mesan auto tartil dan tidak dengan mesin auto tartil.",
+        "Jam dinding yang memiliki desain unik dan ukuran yang relatif kecil.",
     },
     {
       foto: "/assets/jws2.jpeg",
       nama: "Jam Dinding Digital Islami Full Colour",
       harga: 600000,
       tag: "Produk Terbaik",
+      terjual: 125,
       deskripsi:
-        "Jam dinding dengan desain simple minimalis dan full colour. Berukuran 20x40cm, cocok untuk dipasang di ruang keluarga. Tersedia dalam 2 jenis, dengan mesan auto tartil dan tidak dengan mesin auto tartil.",
-    },
-    {
-      foto: "/assets/runningtext2.jpeg",
-      nama: "Running Text",
-      harga: 900000,
-      tag: "Produk Terbaik",
-      deskripsi:
-        "Running text dengan versi yang lebih simple dan tersedia dalam 1 warna saja. Cocok dipasang pada restaurant, masjid, toko, warung pinggir jalan. Bisa berfungsi untuk menampilkan promo, produk, dan lainnya.",
+        "Jam dinding dengan desain simple minimalis dan full colour, berukuran 20x40cm",
     },
     {
       foto: "/assets/runningtext1.jpeg",
       nama: "Running Text Full Colour",
       harga: 1500000,
       tag: "Produk Terbaik",
+      terjual: 17,
       deskripsi:
         "Running text dengan full colour yang sangat cocok dipasang ketika event atau sekedar untuk memperkaya fasilitas.",
+    },
+    {
+      foto: "/assets/jwstv.jpg",
+      nama: "Jam Shalat Waktu TV",
+      harga: 1950000,
+      tag: "Produk Paling Unik",
+      terjual: 17,
+      deskripsi: "Jam Waktu Shalat yang bisa di tampilkan di TV.",
+    },
+    {
+      foto: "/assets/runningtext2.jpeg",
+      nama: "Running Text",
+      harga: 900000,
+      tag: "Produk Terbaik",
+      terjual: 10,
+      deskripsi:
+        "Running text dengan versi yang lebih simple dan tersedia dalam 1 warna saja.",
+    },
+    {
+      foto: "/assets/papanskor.jpeg",
+      nama: "Papan Skor",
+      harga: 1350000,
+      tag: "Produk Terbaik",
+      terjual: 7,
+      deskripsi: "Papan skor berukuran 20x70cm, Menampilkan skor kedua tim.",
+    },
+    {
+      foto: "/assets/mesinautotartil.jpg",
+      nama: "Mesin Auto Tartil",
+      harga: 850000,
+      tag: "Produk Bagus",
+      terjual: 2,
+      deskripsi:
+        "Player TARTIL/TARHIM otomatis menjelang waktu sholat, bisa terintegrasi dengan system audio masjid.",
     },
   ];
 
@@ -92,19 +96,19 @@ export default function Page() {
           {produk.map((produk, i) => (
             <div
               key={`index-${i}`}
-              className="bg-white rounded-xl shadow-xl hover:border border-[#B2212B] w-[90%] sm:w-[450px]"
+              className="bg-white rounded-xl shadow-lg hover:border border-[#B2212B] w-[85%] sm:w-[350px]" // lebih kecil
             >
               <div className="relative">
                 <Image
                   src={produk.foto}
                   alt="foto"
-                  width={500}
-                  height={500}
-                  className="rounded-t-xl object-cover w-full aspect-square shadow-xl"
+                  width={400}
+                  height={400}
+                  className="rounded-t-xl object-cover w-full aspect-square shadow-md"
                 />
                 {/* Tag Badge */}
                 <span
-                  className="absolute top-4 left-4 bg-[#B2212B] text-white text-xs sm:text-lg font-semibold px-3 py-1 rounded-lg shadow-md"
+                  className="absolute top-4 left-4 bg-[#B2212B] text-white text-xs sm:text-sm font-semibold px-2 py-1 rounded-lg shadow-md"
                   style={{
                     textShadow:
                       "0 0 5px #fff, 0 0 10px #ff4d6d, 0 0 20px #ff1e56, 0 0 20px #ff1e56",
@@ -115,28 +119,39 @@ export default function Page() {
                   {produk.tag}
                 </span>
               </div>
-              <div className="p-4 flex flex-col gap-2">
-                <h1 className="font-bold text-lg sm:text-xl break-words">
+              <div className="p-3 flex flex-col gap-2">
+                <h1 className="font-bold text-base sm:text-lg break-words">
                   {produk.nama}
                 </h1>
-                <div className="flex flex-col mb-2">
-                  <p className="text-red-500 text-sm sm:text-lg line-through">
+
+                {/* Harga */}
+                <div className="flex flex-col mb-1">
+                  <p className="text-red-500 text-xs sm:text-sm line-through">
                     Rp. {(produk.harga * 1.5).toLocaleString()}
                   </p>
-                  <p className="text-zinc-800 font-semibold text-lg sm:text-xl">
+                  <p className="text-zinc-800 font-semibold text-base sm:text-lg">
                     Rp. {produk.harga.toLocaleString()}
                   </p>
                 </div>
-                <p className="text-sm sm:text-base text-justify">
+
+                {/* Deskripsi */}
+                <p className="text-xs sm:text-sm text-justify line-clamp-3">
                   {produk.deskripsi}
+                </p>
+
+                {/* Terjual */}
+                <p className="text-lg font-bold text-gray-500 mt-1">
+                  Terjual:{" "}
+                  <span className="font-medium">{produk.terjual}+</span>
                 </p>
               </div>
 
+              {/* Tombol Pesan */}
               <a
                 href="https://wa.me/6285336664466"
                 className="flex justify-end"
               >
-                <button className="bg-[#B2212B] cursor-pointer text-white rounded-lg mx-4 mb-4 px-4 py-2 hover:bg-red-900 ease-in-out duration-300">
+                <button className="bg-[#B2212B] cursor-pointer text-white rounded-lg mx-3 mb-3 px-3 py-1.5 hover:bg-red-900 ease-in-out duration-300 text-sm sm:text-base">
                   Pesan
                 </button>
               </a>
